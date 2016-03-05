dc51149ba32ab609a72479fb5343391e6f634f33
Dave Rapin <dave@rapin.com>
2016-03-05 10:04:57 -0500

    Afeter 9

--- src/index.js
+++ src/index.js
@@ -1,38 +1,97 @@
 require('./styles/index')
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import expect from 'expect.js'
+import deepFreeze from 'deep-freeze'
 
-import counter from 'stores/counter'
-
-const store = createStore(counter)
-
-const Counter = ({
-  value,
-  onIncrement,
-  onDecrement
-}) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-)
-
-const render = () => {
-  ReactDOM.render(
-    <Counter value={store.getState()}
-      onIncrement={() =>
-        store.dispatch({ type: 'INCREMENT' })
-      }
-      onDecrement={() =>
-        store.dispatch({ type: 'DECREMENT' })
-      }
-    />,
-    document.getElementById('app')
-  )
+const addCounter = (list) => {
+  return [...list, 0]
 }
 
-store.subscribe(render)
-render()
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ]
+}
+
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    (list[index] + 1),
+    ...list.slice(index + 1)
+  ]
+}
+
+const testAddCounter = () => {
+  const listBefore = []
+  deepFreeze(listBefore)
+
+  const listAfter = [0]
+  expect(
+    addCounter(listBefore)
+  ).to.eql(listAfter)
+}
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 3, 8]
+  deepFreeze(listBefore)
+
+  const listAfter = [0, 8]
+  expect(
+    removeCounter(listBefore, 1)
+  ).to.eql(listAfter)
+}
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 3, 8]
+  deepFreeze(listBefore)
+
+  const listAfter = [0, 4, 8]
+  expect(
+    incrementCounter(listBefore, 1)
+  ).to.eql(listAfter)
+}
+
+testAddCounter()
+testRemoveCounter()
+testIncrementCounter()
+
+console.log('tests passed')
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { createStore } from 'redux'
+//
+// import counter from 'stores/counter'
+//
+// const store = createStore(counter)
+//
+// const Counter = ({
+//   value,
+//   onIncrement,
+//   onDecrement
+// }) => (
+//   <div>
+//     <h1>{value}</h1>
+//     <button onClick={onIncrement}>+</button>
+//     <button onClick={onDecrement}>-</button>
+//   </div>
+// )
+//
+// const render = () => {
+//   ReactDOM.render(
+//     <Counter value={store.getState()}
+//       onIncrement={() =>
+//         store.dispatch({ type: 'INCREMENT' })
+//       }
+//       onDecrement={() =>
+//         store.dispatch({ type: 'DECREMENT' })
+//       }
+//     />,
+//     document.getElementById('app')
+//   )
+// }
+//
+// store.subscribe(render)
+// render()
